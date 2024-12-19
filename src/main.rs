1506ef494b9ed2f1aa9d7e0f5204f5e1ed7f6f3d
Mitsutaka Shiraishi <srismttk@gmail.com>
2024-12-19 23:01:19 +0900

    3-4, add test for health_check

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use std::net::{Ipv4Addr, SocketAddr};
+use std::{net::{Ipv4Addr, SocketAddr}, os::macos::raw::stat};
 
 use anyhow::Result;
 use axum::{http::StatusCode, routing::get, Router};
 use tokio::net::TcpListener;
+
+/// Test handler health-check that returns `200 OK` status code.
+#[tokio::test]
+async fn health_check_works() {
+    let status_code = health_check().await;
+    assert_eq!(status_code, StatusCode::OK);
+}
 
 /// Handler for health-check, that uses `/health` route, and returns `200 OK` status code.
 pub async fn health_check() -> StatusCode {
