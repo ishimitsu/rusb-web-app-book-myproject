85898025a31b8f774321d39e2c2192268055eebc
Mitsutaka Shiraishi <srismttk@gmail.com>
2024-12-19 22:44:19 +0900

    332-333, add anyhow.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
 use std::net::{Ipv4Addr, SocketAddr};
-use axum::{routing::get, Router};
+
+use anyhow::Result;
+use axum::{http::StatusCode, routing::get, Router};
 use tokio::net::TcpListener;
 
-async fn hell_world() -> &'static str {
-    "Hello, World!"
+/// Handler for health-check, that uses `/health` route, and returns `200 OK` status code.
+pub async fn health_check() -> StatusCode {
+    StatusCode::OK
 }
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/hello", get(hell_world));
-
-    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
-    let listener = TcpListener::bind(addr).await.unwrap();
-
-    println!("Listening on {}", addr);
-
-    axum::serve(listener, app).await.unwrap();
+    let app = Router::new().route("/health", get(health_check));
 }
