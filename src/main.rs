63ff204420c3cca950347c4198dd1b3788987c7e
Mitsutaka Shiraishi <srismttk@gmail.com>
2024-12-19 22:49:50 +0900

    334, implement main that has health-check and return anyhow Error.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,13 @@ pub async fn health_check() -> StatusCode {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let app = Router::new().route("/health", get(health_check));
+
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+    let listener = TcpListener::bind(addr).await?;
+
+    println!("Listening on {}", addr);
+
+    Ok(axum::serve(listener, app).await?)
 }
